92e1b28ad8247350ab7f21083f5e14b8d997c7db
Pengjs <Pengjs@ansteel.com.cn>
2023-02-27 16:34:29 +0800

    update

--- src/App.js
+++ src/App.js
@@ -26,10 +26,22 @@ const App = () => {
     //     </StuContext.Provider>
     //
     // );
-
-    const {data:stus,isSuccess,isLoading}=useGetStudentsQuery();
+    /**
+    currentData:undefined //当前参数的最新数据
+    data:undefined //最新数据
+    isError:false //布尔值，是否有错误
+    error:Error() //对象，有错误时才存在
+    isFetching:true //布尔值，数据是否在加载
+    isLoading:true  //布尔值，数据是否第一次加载
+    isSuccess:false //布尔值，请求是否成功
+    isUninitialized:false //布尔值，请求是否还没有开始发送
+    refetch:f() //一个函数，用来重新加载数据
+    status:"pending" //字符串，请求的状态
+     */
+    const {data:stus,isSuccess,isLoading,refetch }=useGetStudentsQuery();
     return (
         <div>
+            <button onClick={()=>refetch()}>refresh</button>
             {isLoading&&<p>>数据加载中</p>}
             {isSuccess&&<StudentList stus={stus}/>}
         </div>
